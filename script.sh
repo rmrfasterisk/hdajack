05d42eb3314826d7688644dc55183c3939c5cf26
Sai Praneeth Reddy <spreddy617@gmail.com>
2021-06-08 00:11:38 +0530

    don't move. copy instead
    make script.sh executable

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # run the script as root
-mv hda-jack-retask.fw /lib/firmware/hda-jack-retask.fw
+cp hda-jack-retask.fw /lib/firmware/hda-jack-retask.fw
  2>>errors.log
-mv hda-jack-retask.conf /etc/modprobe.d/hda-jack-retask.conf 2>>errors.log
+cp hda-jack-retask.conf /etc/modprobe.d/hda-jack-retask.conf 2>>errors.log
